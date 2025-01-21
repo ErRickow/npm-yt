4158e3d545a6c17bc5d2ccb509de7aa2983c9590
Just R <remixingmagelang@gmail.com>
2025-01-22 05:14:55 +0700

    Update

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended', // Atau aturan lain yang Anda gunakan
+  ],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'], // Tentukan ekstensi di sini
+      rules: {
+        // Aturan khusus untuk file tertentu
+      }
+    }
+  ]
+}
