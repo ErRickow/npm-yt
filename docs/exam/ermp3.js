df0e9c157f6a14225202462a248b9e86eb12d654
ErRickow <ErRickow@users.noreply.github.com>
2025-01-26 19:14:22 +0000

    Prettified Code!

--- docs/exam/ermp3.js
+++ docs/exam/ermp3.js
@@ -1,9 +1,11 @@
-const { ermp3 } = require("@er-npm/scraper")
-//import { ermp3 }
+const { ermp3 } = require('@er-npm/scraper')(
+  //import { ermp3 }
 
-(async () => { //panggil fungsi
-    const url = "https://www.youtube.com/watch?v=vx2u5uUu3DE";
+  async () => {
+    //panggil fungsi
+    const url = 'https://www.youtube.com/watch?v=vx2u5uUu3DE';
     const aud = await ermp3(url);
-    
-    console.log("Done✅:", aud);
-})(); //resultnya berbentuk strings
+
+    console.log('Done✅:', aud);
+  },
+)(); //resultnya berbentuk strings
