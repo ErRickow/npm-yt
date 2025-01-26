3adbab062e862d45dd2ffa257896bb617f6bb901
Just R <remixingmagelang@gmail.com>
2025-01-27 01:38:37 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -269,7 +269,6 @@ async function ermp3(url) {
  * // define the URL
  * const url = "https://Youtube.com/watch"
  * await ermp4(url).then(result => console.log(result)) //ini bakal menampilka resultnya
- * @returns {string} Returns data ErMp4
  *
  * @author ErNewDev0 &lt;ryppain@gmail.com>
  */
