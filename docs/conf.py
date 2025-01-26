88fdf307703b4963f214235d459f5b3eacaa9a56
Just R <remixingmagelang@gmail.com>
2025-01-26 22:28:15 +0700

    update docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,19 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath("../src_for_doc./AutoDocAction"))
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+import json
 
 project = 'Documentation for Er-NPM'
 copyright = '2025, er'
@@ -9,6 +22,13 @@ release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# The full version, including alpha/beta/rc tags
+# read the version from package.json
+with open('../package.json', 'r') as read_file:
+    package = json.load(read_file)
+    release = package['version']
+
+master_doc = 'index'
 
 autosummary_generate = True
 
@@ -34,3 +54,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
