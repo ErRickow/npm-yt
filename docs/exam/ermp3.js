9bbb377c28c04edcd2c083a35458344051544f38
Just R <remixingmagelang@gmail.com>
2025-01-27 02:14:09 +0700

    update docs/exam/ermp3.js

---
+++ docs/exam/ermp3.js
@@ -0,0 +1,9 @@
+const { ermp3 } = require("@er-npm/scraper")
+//import { ermp3 }
+
+(async () => { //panggil fungsi
+    const url = "https://www.youtube.com/watch?v=vx2u5uUu3DE";
+    const aud = await ermp3(url);
+    
+    console.log("Done✅:", aud);
+})(); //resultnya berbentuk strings
