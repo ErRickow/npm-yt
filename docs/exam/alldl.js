2afae3f0a9f57ed2ce70c667866f5cea336f4631
ErRickow <ErRickow@users.noreply.github.com>
2025-01-27 08:01:41 +0000

    Prettified Code!

--- docs/exam/alldl.js
+++ docs/exam/alldl.js
@@ -1,11 +1,11 @@
-const { alldl } = require("@er-npm/scraper");
+const { alldl } = require('@er-npm/scraper');
 //import { alldl } = require("@er-npm/scraper");
 
 (async () => {
-    const url = "https://vm.tiktok.com/ZMkNpBFjX/" //valid url tiktok or any
-    const nya = await alldl(url);
-    
-    for(download of nya) {
-       console.log(`Download ${nya.type} selesai:`, nya.src); //result data from alldl
-    }
+  const url = 'https://vm.tiktok.com/ZMkNpBFjX/'; //valid url tiktok or any
+  const nya = await alldl(url);
+
+  for (download of nya) {
+    console.log(`Download ${nya.type} selesai:`, nya.src); //result data from alldl
+  }
 })();
