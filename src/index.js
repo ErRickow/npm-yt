23f9c5aab81e479f711e06d6370acf1b16567df6
ErRickow <ErRickow@users.noreply.github.com>
2025-01-29 09:44:05 +0000

    Prettified Code!

--- src/index.js
+++ src/index.js
@@ -260,45 +260,45 @@ async function ermp3(url) {
  * @returns {Promise<Object>} Objek yang berisi daftar aplikasi atau pesan kesalahan.
  */
 async function playstore(query) {
-    const url = `https://api.siputzx.my.id/api/apk/playstore?query=${query}`;
-    try {
-        const res = await axios.get(url);
-        const data = res.data.data;
-        
-        if (!Array.isArray(data) || data.length === 0) {
-            return {
-                status: false,
-                why: "Aplikasi tidak ditemukan.",
-                terus_gmna: "Kunjungi: t.me/chakszzz",
-            };
-        }
+  const url = `https://api.siputzx.my.id/api/apk/playstore?query=${query}`;
+  try {
+    const res = await axios.get(url);
+    const data = res.data.data;
 
-        return {
-            status: true,
-            /**
-             * @property {Array<Object>} results - Daftar aplikasi yang ditemukan di Play Store.
-             * @property {string} results[].nama - Nama aplikasi.
-             * @property {string} results[].link - Link Play Store aplikasi.
-             * @property {string} results[].thumb - URL ikon aplikasi.
-             * @property {string} results[].dev - Nama pengembang aplikasi.
-             * @property {string} results[].rating - Rating aplikasi.
-             */
-            results: data.map(app => ({
-                nama: app.nama,
-                link: app.link,
-                thumb: app.img,
-                dev: app.developer,
-                rating: app.rate2,
-            })),
-            from: "@er-npm/scraper",
-        };
-    } catch (error) {
-        return {
-            status: false,
-            why: "Terjadi kesalahan.",
-            terus_gmna: "Kunjungi: t.me/chakszzz",
-        };
+    if (!Array.isArray(data) || data.length === 0) {
+      return {
+        status: false,
+        why: 'Aplikasi tidak ditemukan.',
+        terus_gmna: 'Kunjungi: t.me/chakszzz',
+      };
     }
+
+    return {
+      status: true,
+      /**
+       * @property {Array<Object>} results - Daftar aplikasi yang ditemukan di Play Store.
+       * @property {string} results[].nama - Nama aplikasi.
+       * @property {string} results[].link - Link Play Store aplikasi.
+       * @property {string} results[].thumb - URL ikon aplikasi.
+       * @property {string} results[].dev - Nama pengembang aplikasi.
+       * @property {string} results[].rating - Rating aplikasi.
+       */
+      results: data.map(app => ({
+        nama: app.nama,
+        link: app.link,
+        thumb: app.img,
+        dev: app.developer,
+        rating: app.rate2,
+      })),
+      from: '@er-npm/scraper',
+    };
+  } catch (error) {
+    return {
+      status: false,
+      why: 'Terjadi kesalahan.',
+      terus_gmna: 'Kunjungi: t.me/chakszzz',
+    };
+  }
 }
 
 /**
