f17548c490f511f4b4da9c22eab6333db20dc51b
Just R <remixingmagelang@gmail.com>
2025-02-02 04:54:17 +0700

    update src/bkp/scrape.js

--- src/bkp/scrape.js
+++ src/bkp/scrape.js
@@ -1,56 +1,32 @@
-const cheerio = require('cheerio');
+const dl = require("xnxx-dl");
 
-const axios = require('axios');
-
-const request = axios.default.create({
-  headers: {
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'es-419,es;q=0.5',
-    'Cache-Control': 'max-age=0',
-    'User-Agent':
-      'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36',
-  },
-});
-
-module.exports = async query => {
-  if (!query || typeof query !== 'string') {
-    throw new Error("'query' is missing or not a string");
+async function bokep(judul) {
+  if (!judul) {
+    return {
+      status: false,
+      why: "Masukkan judul"
+    };
   }
-
-  const codedQuery = encodeURIComponent(query);
-  const searchUrl = `https://www.xnxx.com/search/${codedQuery}`;
 
   try {
-    const { data } = await request.get(searchUrl);
-
-    const $ = cheerio.load(data);
-    const anchors = $('.thumb-inside .thumb').find('a');
-
-    if (!anchors || !anchors.length) return;
-
-    const index = Math.floor(Math.random() * anchors.length);
-    const href = anchors.eq(index).attr('href');
-    const url = `https://www.xnxx.com${href}`;
-
-    const { data: content } = await request.get(url);
-
-    const $$ = cheerio.load(content);
-    const video = $$('#video-player-bg').find('script').text();
-
-    const pattern = /html5player\.setVideoUrl(?:High|Low)\('([^']+)'\)/;
-    const link = video.match(pattern)?.[1];
+    const what = await dl.download(judul);
 
     return {
-      judul: $$('.video-title strong').text(),
-      link: url,
-      link_dl: link,
-      value: $$('#video-votes .rating-box.value').text(),
-      views: $$('.metadata').text().split('-')[2]?.trim(),
-      likes: $$('#video-votes .vote-action-good .value').text(),
-      from: '@er-npm/scraper',
+      status: true,
+      judul: what.title,
+      dl_link: what.link_dl,
+      rating: what.value,
+      penonton: what.views,
+      likes: what.likes,
+      from: "@er-npm/scraper"
     };
   } catch (error) {
-    throw error;
+    return {
+      status: false,
+      why: "Error njing.",
+      terus_gmna: "Visit: t.me/chakszzz"
+    };
   }
-};
+}
+
+module.exports = bokep;
