5b424d0e07f9e70593d4d1b04be011737ec3e660
Just R <remixingmagelang@gmail.com>
2025-02-02 04:03:25 +0700

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const {
 const { Innertube, UniversalCache } = require('youtubei.js');
 const { execFile, exec } = require('child_process');
 const ai = require('./ia/index.js');
-const { bkp } = require('./bkp/scrape.js');
+const bkp = require('./bkp/scrape.js');
 const path = require('path');
 const fs = require('fs');
 const os = require('os');
