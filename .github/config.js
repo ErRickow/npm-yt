2fab23f5efde204941e5ee13f02f695a70555d82
Just R <remixingmagelang@gmail.com>
2025-02-14 07:11:02 +0700

    update .github/config.js

--- .github/config.js
+++ .github/config.js
@@ -1,21 +1,25 @@
 'use strict';
 
-const config = require('conventional-changelog-conventionalcommits');
+const config = async () => {
+  const conventionalChangelogConfig = await import('conventional-changelog-conventionalcommits');
 
-module.exports = config({
-  issuePrefixes: ['#'],
-  issueUrlFormat: 'https://github.com/ErRickow/npm-yt/issues/{{id}}',
-  commitUrlFormat: 'https://github.com/ErRickow/npm-yt/commit/{{hash}}',
-  userUrlFormat: 'https://github.com/{{user}}',
-  types: [
-    { type: 'feat', section: 'Features' },
-    { type: 'fix', section: 'Bug Fixes' },
-    { type: 'docs', section: 'Documentation' },
-    { type: 'style', section: 'Styles' },
-    { type: 'refactor', section: 'Code Refactoring' },
-    { type: 'perf', section: 'Performance Improvements' },
-    { type: 'test', section: 'Tests' },
-    { type: 'chore', section: 'Chores' },
-    { type: 'revert', section: 'Reverts' }
-  ]
-});
+  return conventionalChangelogConfig.default({
+    issuePrefixes: ['#'],
+    issueUrlFormat: 'https://github.com/ErRickow/npm-yt/issues/{{id}}',
+    commitUrlFormat: 'https://github.com/ErRickow/npm-yt/commit/{{hash}}',
+    userUrlFormat: 'https://github.com/{{user}}',
+    types: [
+      { type: 'feat', section: 'Features' },
+      { type: 'fix', section: 'Bug Fixes' },
+      { type: 'docs', section: 'Documentation' },
+      { type: 'style', section: 'Styles' },
+      { type: 'refactor', section: 'Code Refactoring' },
+      { type: 'perf', section: 'Performance Improvements' },
+      { type: 'test', section: 'Tests' },
+      { type: 'chore', section: 'Chores' },
+      { type: 'revert', section: 'Reverts' }
+    ]
+  });
+};
+
+module.exports = config;
