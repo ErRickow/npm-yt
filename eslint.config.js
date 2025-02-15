b71937cb4e4c4254606f2c72985f49c73d9aa237
Just R <remixingmagelang@gmail.com>
2025-02-16 04:36:31 +0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,30 @@
-const js = require('@eslint/js');
-const prettier = require('eslint-plugin-prettier');
-
-module.exports = [
-  js.configs.recommended,
-  {
-    plugins: { prettier },
-    rules: {
-      'prettier/prettier': ['error', { semi: true }],
-      semi: ['error', 'always']
-    }
-  }
-];
+module.exports = {
+  env: {
+    node: true, // Aktifkan lingkungan Node.js
+    es2021: true, // Aktifkan fitur ES2021
+  },
+  overrides: [
+    {
+      files: ["*.mjs"], // Semua file .mjs dianggap ES Modules
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+    {
+      files: ["*.cjs"], // Semua file .cjs dianggap CommonJS
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.js"], // Jika ada .js, deteksi otomatis berdasarkan package.json
+      parserOptions: {
+        sourceType: "unambiguous",
+      },
+    },
+  ],
+  rules: {
+    "no-undef": "off", // Hindari error undefined untuk global variables
+    "no-unused-vars": "warn", // Tandai variabel yang tidak terpakai
+  },
+};
