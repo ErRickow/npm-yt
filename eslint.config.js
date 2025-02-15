13d4412ff55b9aa530a973d69b225f3aa9665683
Just R <remixingmagelang@gmail.com>
2025-02-16 04:14:04 +0700

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
-  rules: {
-    'prettier/prettier': ['error', { semi: true }], // Pastikan Prettier mengikuti aturan ini
-    semi: ['error', 'always'] // ESLint akan memaksa penggunaan semicolon
-  },
-  plugins: ['prettier']
-};
+import js from "@eslint/js";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: { prettier },
+    rules: {
+      "prettier/prettier": ["error", { semi: true }],
+      "semi": ["error", "always"]
+    }
+  }
+];
