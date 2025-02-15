79fe3d34caaa507f75214fd13c868f68a02c058e
Just R <remixingmagelang@gmail.com>
2025-02-16 04:10:08 +0700

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,11 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier' // Tambahkan ini agar tidak bentrok dengan Prettier
+    'prettier'
   ],
   rules: {
-    'prettier/prettier': ['error']
+    'prettier/prettier': ['error', { semi: true }], // Pastikan Prettier mengikuti aturan ini
+    'semi': ['error', 'always'] // ESLint akan memaksa penggunaan semicolon
   },
   plugins: ['prettier']
-}
+};
