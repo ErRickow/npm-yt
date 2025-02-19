34114003a88ab5c1bbe9d342a6aeb55dc22afb25
Just R <remixingmagelang@gmail.com>
2025-02-20 02:45:15 +0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,32 @@ const js = require('@eslint/js');
 const prettier = require('eslint-plugin-prettier');
 const unusedImports = require('eslint-plugin-unused-imports');
 
-module.exports = {
-  extends: [js.configs.recommended], // Menggunakan extends agar lebih rapi
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: {
-      window: 'readonly',
-      document: 'readonly',
-      require: 'readonly', // Diperlukan agar ESLint tidak error
-      console: 'readonly', // Diperlukan agar console.log tidak dianggap error
-      process: 'readonly', // Untuk environment Node.js
-      module: 'readonly' // Agar require/module.exports tidak error
+module.exports = [
+  js.configs.recommended, // Menggunakan konfigurasi JS yang direkomendasikan
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        require: 'readonly', // Diperlukan agar ESLint tidak error
+        console: 'readonly', // Diperlukan agar console.log tidak dianggap error
+        process: 'readonly', // Untuk environment Node.js
+        module: 'readonly' // Agar require/module.exports tidak error
+      }
+    },
+    plugins: {
+      prettier,
+      'unused-imports': unusedImports
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'off', // Gunakan plugin unused-imports
+      'unused-imports/no-unused-vars': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+      'no-console': 'off', // Izinkan console.log
+      'no-undef': 'off' // Matikan aturan no-undef karena kita sudah atur globals
     }
-  },
-  plugins: ['prettier', 'unused-imports'], // Memastikan plugins hanya dalam array
-  rules: {
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'off', // Gunakan plugin unused-imports
-    'unused-imports/no-unused-vars': 'warn',
-    'unused-imports/no-unused-imports': 'error',
-    'no-console': 'off', // Izinkan console.log
-    'no-undef': 'off' // Matikan aturan no-undef karena kita sudah atur globals
-  },
-  fix: true // Memastikan auto-fix saat ESLint dijalankan
-};
+  }
+];
